5c39cdc1d6de652aa14e1cdd389b333ffce5f7c5
Chin Kai Xiang <chinkx1205@gmail.com>
2020-03-18 01:24:47 +0800

    Created database tables

---
+++ database/migrations/2020_03_17_170851_create_candidates_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCandidatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('name', 100)->index();
+            $table->unsignedInteger('party_id');
+            $table->timestamps();
+            $table->foreign('party_id')
+                ->references('id')->on('parties');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('candidates');
+    }
+}
